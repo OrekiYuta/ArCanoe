cea76693f60a0af68f5204eba810cc67b2840ca9
OrekiYuta <orekiyuta@gmail.com>
2019-10-23 00:42:21 +0800

    Preliminary completion details page

---
+++ src/main/java/cn/orekiyuta/ark/controller/QuestionController.java
@@ -0,0 +1,28 @@
+package cn.orekiyuta.ark.controller;
+
+import cn.orekiyuta.ark.dto.QuestionDTO;
+import cn.orekiyuta.ark.mapper.QuestionMapper;
+import cn.orekiyuta.ark.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Created by orekiyuta on  2019/10/22 - 18:26
+ **/
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    public String question(@PathVariable(name="id") Integer id,
+                    Model model){
+        QuestionDTO questionDTO=questionService.getById(id);
+        model.addAttribute("question",questionDTO);
+        return "question";
+    }
+}
