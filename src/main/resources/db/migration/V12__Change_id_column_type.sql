1ed52643ed4cbc5bd25121f05d354549f49b1ce2
OrekiYuta <orekiyuta@gmail.com>
2019-12-26 02:23:55 +0800

    Modify Configuration parameter

--- src/main/resources/db/migration/V12__Change_id_column_type.sql
+++ src/main/resources/db/migration/V12__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table question alter column id bigint auto_increment  not null;
-alter table user alter column id bigint auto_increment  not null;
+alter table question modify column id bigint auto_increment  not null;
+alter table user modify column id bigint auto_increment  not null;
